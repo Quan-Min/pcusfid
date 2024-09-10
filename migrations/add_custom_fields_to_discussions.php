08de81f12407a2e56d0138083b25fb6263b84063
Wppd-CC <964005200@qq.com>
2024-09-10 23:43:55 +0800

    add

--- migrations/add_custom_fields_to_discussions.php
+++ migrations/add_custom_fields_to_discussions.php
@@ -4,7 +4,7 @@ use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 return Migration::addColumns('posts', [
-    'c_uindex' => ['string', 'nullable' => true],
-    'c_olink' => ['string', 'nullable' => true],
-    'c_slink' => ['string', 'nullable' => true],
+    'c_uindex' => ['type' => 'string', 'length' => 255, 'nullable' => true],
+    'c_olink' => ['type' => 'string', 'length' => 255, 'nullable' => true],
+    'c_slink' => ['type' => 'string', 'length' => 255, 'nullable' => true],
 ]);
